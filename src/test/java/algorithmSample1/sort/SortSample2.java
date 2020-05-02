220beb042752d50dae1ad5e893fb5a05fb540fd6
jayjun <jayjun.ryu@kakaocorp.com>
2020-05-02 17:51:54 +0900

    기본 정렬 알고리즘 테스트 코드 추가 List 문자열 정렬 테스트 코드 추가

--- src/test/java/algorithmSample1/sort/SortSample2.java
+++ src/test/java/algorithmSample1/sort/SortSample2.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -71,6 +72,30 @@ public class SortSample2 {
         System.out.println(Arrays.toString(strings));
 //[zzz, zazzy, z, y, x, abc]
         assertArrayEquals(expected3.toArray(), strings);
+    }
+
+    @Test
+    public void String_List_정렬(){
+        List<String> list = Arrays.asList("1", "2", "3", "A", "B", "C", "a", "b", "c");
+
+        List<String> sortedList = list.stream()
+                .sorted(Comparator.naturalOrder())
+                .collect(Collectors.toList());
+
+        //[1, 2, 3, A, B, C, a, b, c]
+        System.out.println(Arrays.toString(sortedList.toArray()));
+
+        List<String> sortedList1 = list.stream()
+                .sorted((o1,o2)-> o1.compareTo(o2))
+                .collect(Collectors.toList());
+
+        //[1, 2, 3, A, B, C, a, b, c]
+        System.out.println(Arrays.toString(sortedList1.toArray()));
+
+        List<String> sortedList3 = list.stream().sorted().collect(Collectors.toList());
+
+        //[1, 2, 3, A, B, C, a, b, c]
+        System.out.println(Arrays.toString(sortedList3.toArray()));
     }
 
 
